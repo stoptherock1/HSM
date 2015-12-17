9a47910a9a22e0cc3f1f59b6eef1bde04dc8f145
stoptherock1 <stoptherock1@gmail.com>
2015-12-17 19:20:17 +0100

    Mockups are integrated to the project

--- code/qtApp/main.cpp
+++ code/qtApp/main.cpp
@@ -1,7 +1,13 @@
 #include <QApplication>
-#include "browseWindow.h"
+#include <browseWindow.h>
 #include <loginWindow.h>
-#include "hsmMetaData.h"
+#include <hsmMetaData.h>
+#include <bookingpage.h>
+#include <fistpage.h>
+#include <roomavailability.h>
+#include <staffpage.h>
+#include <updateinformation.h>
+
 
 int main(int argc, char *argv[])
 {
@@ -10,8 +16,26 @@ int main(int argc, char *argv[])
 
     viewParameters parameters;
 
+
+
+//    bookingPage window1;
+//    window1.exec();
+
+//    fistpage window2;
+//    window2.show();
+
+//    roomAvailability window3;
+//    window3.exec();
+
+//    staffPage window4;
+//    window4.exec();
+
+//    updateInformation window5;
+//    window5.exec();
+
 //    loginWindow login(0, &parameters);
 //    login.exec();
+
 
     browseWindow browse(0, &parameters);
     browse.show();
--- code/qtApp/qtApp.pro
+++ code/qtApp/qtApp.pro
@@ -16,18 +16,35 @@ SOURCES += main.cpp\
     databaseConnection.cpp \
     loginWindow.cpp \
     browseWindow.cpp \
-    availableRoomsModel.cpp
+    availableRoomsModel.cpp \
+    bookingpage.cpp \
+    fistpage.cpp \
+    roomavailability.cpp \
+    simpleClass.cpp \
+    staffpage.cpp \
+    updateinformation.cpp
 
 HEADERS  += \
     databaseConnection.h \
     loginWindow.h \
     browseWindow.h \
     hsmMetaData.h \
-    availableRoomsModel.h
+    availableRoomsModel.h \
+    bookingpage.h \
+    fistpage.h \
+    roomavailability.h \
+    simpleClass.h \
+    staffpage.h \
+    updateinformation.h
 
 FORMS    += \
     loginWindow.ui \
-    browseWindow.ui
+    browseWindow.ui \
+    bookingpage.ui \
+    fistpage.ui \
+    roomavailability.ui \
+    staffpage.ui \
+    updateinformation.ui
 
 CONFIG += c++11
 
